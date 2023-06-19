e31bc0de716ab44656a4f177b662d2043f7c8b3e
Andrew <ahing910@gmail.com>
2023-06-19 14:51:41 -0400

    Displaying critic score

---
+++ src/components/gameCard.tsx
@@ -0,0 +1,27 @@
+import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
+import { Game } from "../hooks/useGames";
+import PlatformIconList from "./platformIconList";
+import CriticScore from "./CriticScore";
+
+interface Props {
+  game: Game;
+}
+
+const GameCard = ({ game }: Props) => {
+  return (
+    <Card borderRadius={10} overflow="hidden">
+      <Image src={game.background_image}></Image>
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+        <HStack justifyContent= 'space-between'>
+            <PlatformIconList
+            platform={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic}></CriticScore>
+        </HStack>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCard;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api/',
+    baseURL: 'https://api.rawg.io/api',
     params:{
         key: '1f515dda61854f98a213ac2399b4e3c9'
     }
