e56ee721a5c0ee97ca2a530a757cd4a05e476335
Andrew <ahing910@gmail.com>
2023-06-26 17:56:12 -0400

    Refactoring fetch games with react query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,7 @@
-import genres from "../data/genre.ts";
+import { useQuery } from '@tanstack/react-query'
+import apiClient from "../services/api-client.ts";
+import { FetchResponse } from '../services/api-client.ts';
+import genre from '../data/genre.ts';
 
 export interface Genre {
     id: number;
@@ -6,6 +9,15 @@ export interface Genre {
     image_background: string;
 }
 
-const useGenres = () => ({data: genres, isLoading: false, error: null});
+const useGenres = () => useQuery({
+    queryKey: ['genres'],
+    queryFn: () => 
+        apiClient
+            .get<FetchResponse<Genre>>('/genres')
+            .then(res => res.data),
+    staleTime: 24 * 60 * 60 * 1000,
+    initialData: {count : genre.length, results: genre}
+    
+});
 ;
 export default useGenres;
